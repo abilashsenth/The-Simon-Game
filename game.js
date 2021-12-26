b01fe25f6a0a61e649226aff20cc6efb501d5db1
abilashsenth <abilashsenth@gmail.com>
2021-12-26 19:05:50 +0530

    first commit

---
+++ game.js
@@ -0,0 +1,85 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var firstKeyPress = true;
+var level = 0;
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  $("#level-title").text("Level " + level);
+
+  playSound(randomChosenColor);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  firstKeyPress = true;
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game over, Press any key to restart");
+    startOver();
+  }
+}
+
+$(".btn").on("click", function () {
+  var userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$("#level-title").on("click", function(){
+    if(firstKeyPress){
+        firstKeyPress = false;
+        nextSequence();
+    }
+})
+
+$(document).on("keypress", function (event) {
+  if (firstKeyPress) {
+    firstKeyPress = false;
+    nextSequence();
+  }
+});
